72f3a07022278660c694f3e6b4631c753adfebec
Alexander Frederik Vreeken <alexanderfrederikvreeken@macbook-pro.home>
2020-11-11 19:33:04 +0100

    Added the Stripe api version according to the docs.

--- components/Product.js
+++ components/Product.js
@@ -1,11 +1,15 @@
 import moment from 'moment'
 import useSWR from 'swr'
+import { loadStripe } from "@stripe/stripe-js";
 import { request } from 'graphql-request'
 
 import Button from './Button'
 import Line from './Line'
 
 import styles from '../styles/Product.module.css'
+
+const { NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY } = process.env
+const stripePromise = loadStripe(`${NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY}`)
 
 const READ_PRODUCT_QUERY = /* GraphQL */ `
     query($_id: ID!) {
@@ -34,8 +38,26 @@ const productFetcher = (query, _id) => request('/api/products', query, { _id })
 export default function Product({ _id }) {
     const { data, error } = useSWR([READ_PRODUCT_QUERY, _id], productFetcher)
 
-    const handleCheckout = () => {
+    const handleCheckout = async (product) => {
         console.log('Handle checkout!')
+        const stripe = await stripePromise;
+
+        const response = await fetch('/api/checkout', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(product)
+        })
+        const session = await response.json()
+
+        const result = await stripe.redirectToCheckout({
+            sessionId: session.id
+        })
+
+        if (result.error) {
+        
+        }
     }
 
     const renderProduct = (product) => {
@@ -60,7 +82,7 @@ export default function Product({ _id }) {
                 <Line />
                 <div className={styles.product__pricing}>
                     <h3>{`${product.price.amount} ${product.price.currency}`}</h3>
-                    <Button background='#0076ff' border='#0076ff' color='white' hoverBackground='white' hoverBorder='#0076ff' hoverColor='#0076ff' onClick={() => handleCheckout()} text='Buy Now' type='button' />
+                    <Button background='#0076ff' border='#0076ff' color='white' hoverBackground='white' hoverBorder='#0076ff' hoverColor='#0076ff' onClick={() => handleCheckout(product)} text='Buy Now' type='button' />
                 </div>
             </div>
         )
--- pages/api/checkout.js
+++ pages/api/checkout.js
@@ -1,7 +1,30 @@
+const { STRIPE_SECRET_KEY } = process.env
 
-export default function handler(req, res) {
+const stripe = require('stripe')(`${STRIPE_SECRET_KEY}`)
+stripe.api_version = '2020-08-27'
+
+export default async function handler(req, res) {
     // res.statusCode = 200
     // res.setHeader('Content-Type', 'application/json')
     // res.end(JSON.stringify({ name: 'John Doe' }))
-    
+    const product = req.body
+    const session = await stripe.checkout.sessions.create({
+        payment_method_types: ['ideal'],
+        line_items: [{
+            price_data: {
+                currency: product.price.currency,
+                product_data: {
+                    name: product.title,
+                    // images: [product.photo],
+                },
+                unit_amount: product.price.amount * 100,
+            },
+            quantity: 1,
+        }],
+        mode: 'payment',
+        success_url: `${req.headers.origin}?success=true`,
+        cancel_url: `${req.headers.origin}?canceled=true`,
+    })
+
+    res.json({ id: session.id })
 }
